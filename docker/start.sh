bb7123f152b978af0f6102e5b74fe8408cc87b04
Abhilash R <abhilash.kollam@gmail.com>
2025-09-28 11:55:31 +0530

    adding starup script

---
+++ docker/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# The directory to check for in the root directory
+BMSHELL_DIR="/bmcshellhome"
+
+# The location of your JAR file
+JAR_PATH="/root/bmcshell/target/bmcshell-0.0.1-SNAPSHOT.jar"
+
+# Check if the directory exists. The -d flag tests for a directory.
+if [ -d "$BMSHELL_DIR" ]; then
+    echo "Directory '$BMSHELL_DIR' exists. Changing directory..."
+    cd "$BMSHELL_DIR"
+else
+    echo "Directory '$BMSHELL_DIR' not found. Creating and changing directory..."
+    mkdir -p "$BMSHELL_DIR" # -p creates parent directories if needed.
+    cd "$BMSHELL_DIR"
+fi
+
+# Run the Java application from the new working directory
+echo "Executing Java application..."
+ollama serve &
+java -jar "$JAR_PATH"
+
