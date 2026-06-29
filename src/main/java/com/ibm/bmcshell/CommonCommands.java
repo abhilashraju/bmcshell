6a8e093abea721f877217cac7e73e7732633cbcb
Abhilash R <abhilash.kollam@gmail.com>
2026-06-29 17:15:47 +0530

    fixing menu display on back selection

--- src/main/java/com/ibm/bmcshell/CommonCommands.java
+++ src/main/java/com/ibm/bmcshell/CommonCommands.java
@@ -998,11 +998,12 @@ public class CommonCommands implements ApplicationContextAware {
                     endPoints.push(Util.sorted(Util.buildLinksAndTargets(new ObjectMapper().readTree(resp))));
                     endPoints.peek().add(0, new Util.EndPoints("back", "Get"));
                 }
-                displayCurrent();
+
             }
         } else {
             defaultOutputConsumer().accept("No response");
         }
+        displayCurrent();
 
     }
 
